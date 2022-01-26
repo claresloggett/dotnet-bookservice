76e2057e3c40f580ae27d7f5ebe938bbfbb9034b
Clare Sloggett <claresloggett@gmail.com>
2022-01-26 22:45:15 +1100

    More controller methods
    Looks like now the Book post method optionally accepts the entire Author child object

--- bookservice_api/Controllers/AuthorsController.cs
+++ bookservice_api/Controllers/AuthorsController.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using bookservice_api.Models;
 using DefaultNamespace;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace bookservice_api.Controllers;
 
@@ -22,4 +24,68 @@ public class AuthorsController : Controller
         IEnumerable<Author> authors = _context.Authors;
         return Ok(authors);
     }
+
+    [HttpGet("{id}")]
+     public async Task<ActionResult<Author>> GetAuthor(int id)
+     {
+         Author author = await _context.Authors.FindAsync(id);
+         if (author == null)
+         {
+             return NotFound();
+         }
+    
+         return Ok(author);
+     }
+
+     [HttpPost]
+     public async Task<IActionResult> PostAuthor(Author author)
+     {
+         if (!ModelState.IsValid)
+         {
+             return BadRequest(ModelState);
+         }
+         
+         _context.Authors.Add(author);
+         await _context.SaveChangesAsync();
+
+         // Tutorial used CreatedAtRoute. Here, could also just use "GetAuthor"
+         return CreatedAtAction(nameof(GetAuthor), new { id = author.Id }, author);
+     }
+     
+     [HttpPut("{id}")]
+     public async Task<IActionResult> PutAuthor(int id, Author author)
+     {
+         if (!ModelState.IsValid)
+         {
+             return BadRequest(ModelState);
+         }
+
+         if (id != author.Id)
+         {
+             return BadRequest();
+         }
+
+         _context.Entry(author).State = EntityState.Modified;
+
+         // NB have left out DbUpdateConcurrencyException catching here
+         await _context.SaveChangesAsync();
+         
+         return NoContent();
+     }
+     
+     [HttpDelete("{id}")]
+     public async Task<IActionResult> DeleteAuthor(int id)
+     {
+         Author author = await _context.Authors.FindAsync(id);
+         if (author == null)
+         {
+             return NotFound();
+         }
+
+         _context.Authors.Remove(author);
+         await _context.SaveChangesAsync();
+
+         return Ok(author);
+     }
+     
 }
--- bookservice_api/Controllers/BooksController.cs
+++ bookservice_api/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using bookservice_api.Models;
 using DefaultNamespace;
 using Microsoft.AspNetCore.Mvc;
@@ -18,9 +19,72 @@ public class BooksController : Controller
     }
     
     [HttpGet]
-    public ActionResult<IEnumerable<Author>> GetAuthors()
+    public ActionResult<IEnumerable<Book>> GetBooks()
     {
         IEnumerable<Book> books = _context.Books;   //.Include(b => b.Author);
         return Ok(books);
     }
+    
+    [HttpGet("{id}")]
+         public async Task<ActionResult<Book>> GetBook(int id)
+         {
+             Book book = await _context.Books.FindAsync(id);
+             if (book == null)
+             {
+                 return NotFound();
+             }
+        
+             return Ok(book);
+         }
+
+     [HttpPost]
+     public async Task<IActionResult> PostBook(Book book)
+     {
+         if (!ModelState.IsValid)
+         {
+             return BadRequest(ModelState);
+         }
+         
+         _context.Books.Add(book);
+         await _context.SaveChangesAsync();
+
+         // Tutorial used CreatedAtRoute. Here, could also just use "GetBook"
+         return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+     }
+     
+     [HttpPut("{id}")]
+     public async Task<IActionResult> PutBook(int id, Book book)
+     {
+         if (!ModelState.IsValid)
+         {
+             return BadRequest(ModelState);
+         }
+
+         if (id != book.Id)
+         {
+             return BadRequest();
+         }
+
+         _context.Entry(book).State = EntityState.Modified;
+
+         // NB have left out DbUpdateConcurrencyException catching here
+         await _context.SaveChangesAsync();
+         
+         return NoContent();
+     }
+     
+     [HttpDelete("{id}")]
+     public async Task<IActionResult> DeleteBook(int id)
+     {
+         Book book = await _context.Books.FindAsync(id);
+         if (book == null)
+         {
+             return NotFound();
+         }
+
+         _context.Books.Remove(book);
+         await _context.SaveChangesAsync();
+
+         return Ok(book);
+     }
 }
